72bdf9ed2841006227b2cca6c412708510367e4a
Alexey Sosnoviy <int-it@yandex.ru>
2020-05-15 19:58:08 +0300

    mdo bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
     }
 
     implementation("com.github.1c-syntax", "utils", "4034e83681b")
-    implementation("com.github.1c-syntax", "mdclasses", "86be1579c4")
+    implementation("com.github.1c-syntax", "mdclasses", "9051479ef4")
 
     compileOnly("org.projectlombok", "lombok", lombok.version)
 
